b1b5a86d9bf7a32bc16100c938525026d8025acc
alfarissy040 <alfarissy040@gmail.com>
2023-03-03 22:15:24 +0700

    add next_secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export const authOptions = {
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         }),
     ],
+    secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
