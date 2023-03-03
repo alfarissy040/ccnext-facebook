c21e510ead10c19c51d1b2f3f91c8eb074034ce8
alfarissy040 <alfarissy040@gmail.com>
2023-03-03 22:34:20 +0700

    fix import error

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
     return (
         <div className="w-full min-h-screen grid place-content-center">
             <div className="flex flex-col justify-center gap-y-4">
-                <Image src={"https://links.papareact.com/5me"} width={100} height={100} alt="Facebook icon" className="object-contain mx-auto" />
+                <img src={"https://links.papareact.com/5me"} width={100} height={100} alt="Facebook icon" className="object-contain mx-auto" />
                 <h1 className="px-5 py-2 text-white text-center rounded-full shadow cursor-pointer bg-blue-600" onClick={() => signIn()}>
                     Login with Facebook
                 </h1>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import nextAuth from "next-auth";
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
     // Configure one or more authentication providers
