192c6fa77bac01b130201bff49a92da01ab4e497
alfarissy040 <alfarissy040@gmail.com>
2023-03-06 14:51:01 +0700

    update login with github

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,15 +1,16 @@
 import { signIn } from "next-auth/react";
-import Image from "next/image";
-import React from "react";
 
 const Login = () => {
     return (
         <div className="w-full min-h-screen grid place-content-center">
             <div className="flex flex-col justify-center gap-y-4">
                 <img src={"https://links.papareact.com/5me"} width={100} height={100} alt="Facebook icon" className="object-contain mx-auto" />
-                <h1 className="px-5 py-2 text-white text-center rounded-full shadow cursor-pointer bg-blue-600" onClick={() => signIn()}>
+                <button className="px-5 py-2 text-white text-center shadow bg-blue-600" onClick={() => signIn("facebook")}>
                     Login with Facebook
-                </h1>
+                </button>
+                <button className="px-5 py-2 text-white text-center shadow bg-zinc-900" onClick={() => signIn("github")}>
+                    Login with Github
+                </button>
             </div>
         </div>
     );
